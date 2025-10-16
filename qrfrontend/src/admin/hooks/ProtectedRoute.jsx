ac24bb1588bc08de78ea6b3539093d215bb24d4e
KPRAHUL1 <rahulsachin00007@gmail.com.com>
2025-10-16 20:41:40 +0530

    update door functionality

--- qrfrontend/src/admin/hooks/ProtectedRoute.jsx
+++ qrfrontend/src/admin/hooks/ProtectedRoute.jsx
@@ -1,12 +1,9 @@
-// components/ProtectedRoute.js
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
   const token = localStorage.getItem('adminToken');
 
-  // If there's a token, render the child component (e.g., AdminDashboard)
-  // Otherwise, redirect to the login page
   return token ? <Outlet /> : <Navigate to="/admin/login" />;
 };
 
